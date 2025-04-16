660321c33787abc69c3726f0d9dcf861f5b3c44c
JOSEPHAT (M) NDUNGURU <ndungurujosephat30@gmail.com>
2025-04-16 07:53:02 +0300

    Create Task.php

---
+++ app/Models/Task.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'due_date',
+        'completed',
+        'priority',
+        'category',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'due_date' => 'date',
+        'completed' => 'boolean',
+    ];
+}
