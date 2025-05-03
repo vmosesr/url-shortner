b26f47d4278593c398dfdf0be5d9ff6486b312c4
JOSEPHAT (M) NDUNGURU <ndungurujosephat30@gmail.com>
2025-05-03 17:59:05 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -24,21 +24,6 @@ Route::get('/shorten-home', [UrlShortenerController::class, 'linker'])->name('sh
 Route::post('/shorten', [UrlShortenerController::class, 'shorten'])->name('shortener.shorten');
 Route::get('/{code}', [UrlShortenerController::class, 'redirect'])->name('shortener.redirect');
 
-//todo
-// Route::resource('/tasks', TaskController::class);
-
-// Define routes explicitly instead of using resource
-
-Route::get('/tasks-home', [TaskController::class, 'index'])->name('tasks-home');
-Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
-Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
-Route::get('/tasks/{task}', [TaskController::class, 'show'])->name('tasks.show');
-Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
-Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
-Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
-
-
-Route::get('/test', [TestController::class, 'test']);
 
 // admin
 Route::view('admin/dashboard', 'admin.dashboard')
